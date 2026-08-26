30ca3cfef17edecd12f983f6121eb16805e1f20d
av-dev2 <emgani@aakvatech.com>
2026-08-26 13:14:29 +0300

    test: cover the Bay master

--- servicems/service_management/doctype/bay/test_bay.py
+++ servicems/service_management/doctype/bay/test_bay.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2024, Aakvatech Limited and Contributors
 # See license.txt
 
-# import frappe
-from frappe.tests.utils import FrappeTestCase
+import frappe
+
+from servicems.tests.base import ServiceMSTestCase
+from servicems.tests.utils import BAY, IGNORE_TEST_RECORD_DEPENDENCIES, WORKSHOP
+
+IGNORE_TEST_RECORD_DEPENDENCIES = IGNORE_TEST_RECORD_DEPENDENCIES
 
 
-class TestBay(FrappeTestCase):
-	pass
+class TestBay(ServiceMSTestCase):
+	def test_is_named_by_bay_field(self):
+		bay = frappe.get_doc("Bay", BAY)
+		self.assertEqual(bay.bay, BAY)
+		self.assertEqual(bay.service_workshop, WORKSHOP)
+
+	def test_unknown_workshop_is_rejected(self):
+		doc = frappe.get_doc(
+			{"doctype": "Bay", "bay": "_Test SMS Bay Bad", "service_workshop": "_Test SMS Missing"}
+		)
+		self.assertRaises(frappe.LinkValidationError, doc.insert)
+
+	def test_duplicate_bay_is_rejected(self):
+		doc = frappe.get_doc({"doctype": "Bay", "bay": BAY, "service_workshop": WORKSHOP})
+		self.assertRaises(frappe.DuplicateEntryError, doc.insert)
