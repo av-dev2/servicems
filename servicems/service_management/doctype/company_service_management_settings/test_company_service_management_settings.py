b08236fa9c5e9c0d37e3f3e63ad56492b508e6ab
av-dev2 <emgani@aakvatech.com>
2026-08-26 13:14:31 +0300

    test: cover Company Service Management Settings

--- servicems/service_management/doctype/company_service_management_settings/test_company_service_management_settings.py
+++ servicems/service_management/doctype/company_service_management_settings/test_company_service_management_settings.py
@@ -1,9 +1,20 @@
 # Copyright (c) 2022, Aakvatech Limited and Contributors
 # See license.txt
 
-# import frappe
-import unittest
+import frappe
+
+from servicems.tests.base import ServiceMSTestCase
+from servicems.tests.utils import COMPANY, IGNORE_TEST_RECORD_DEPENDENCIES
+
+IGNORE_TEST_RECORD_DEPENDENCIES = IGNORE_TEST_RECORD_DEPENDENCIES
 
 
-class TestCompanyServiceManagementSettings(unittest.TestCase):
-	pass
+class TestCompanyServiceManagementSettings(ServiceMSTestCase):
+	def test_named_by_company(self):
+		settings = frappe.get_doc("Company Service Management Settings", COMPANY)
+		self.assertEqual(settings.company, COMPANY)
+		self.assertIn(settings.use_parts_entry, (0, 1))
+
+	def test_one_record_per_company(self):
+		doc = frappe.get_doc({"doctype": "Company Service Management Settings", "company": COMPANY})
+		self.assertRaises(frappe.DuplicateEntryError, doc.insert)
