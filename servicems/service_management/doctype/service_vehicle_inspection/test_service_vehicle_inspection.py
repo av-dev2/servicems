325dc75dab6443fa385852aa9e5ccd18c71788d5
av-dev2 <emgani@aakvatech.com>
2026-08-26 13:14:17 +0300

    test: cover Service Vehicle Inspection checklists

--- servicems/service_management/doctype/service_vehicle_inspection/test_service_vehicle_inspection.py
+++ servicems/service_management/doctype/service_vehicle_inspection/test_service_vehicle_inspection.py
@@ -1,9 +1,40 @@
 # Copyright (c) 2022, Aakvatech Limited and Contributors
 # See license.txt
 
-# import frappe
-import unittest
+import frappe
+from frappe.utils import nowdate
+
+from servicems.tests.base import ServiceMSTestCase
+from servicems.tests.utils import IGNORE_TEST_RECORD_DEPENDENCIES, VEHICLE_TYPE
+
+IGNORE_TEST_RECORD_DEPENDENCIES = IGNORE_TEST_RECORD_DEPENDENCIES
+
+CHECKLISTS = {
+	"service_lighting_checklist": {"lighting_check_item": "Head lamps", "lighting_mark": 1},
+	"service_brake_checklist": {"brake_system": "Pads", "mark": 1, "brake_remarks": "OK"},
+}
 
 
-class TestServiceVehicleInspection(unittest.TestCase):
-	pass
+class TestServiceVehicleInspection(ServiceMSTestCase):
+	def test_autoname_and_checklists(self):
+		doc = frappe.get_doc(
+			{
+				"doctype": "Service Vehicle Inspection",
+				"date": nowdate(),
+				"driver_name": "Driver",
+				"vehicle_plate_number": "T 123",
+				"mileage": "1000",
+				"service_vehicle_type": VEHICLE_TYPE,
+				**{fieldname: [row] for fieldname, row in CHECKLISTS.items()},
+			}
+		).insert()
+		self.assertTrue(doc.name.startswith("SVI"))
+		self.assertEqual(doc.service_lighting_checklist[0].lighting_check_item, "Head lamps")
+		self.assertEqual(doc.service_brake_checklist[0].mark, 1)
+
+	def test_every_checklist_table_exists(self):
+		meta = frappe.get_meta("Service Vehicle Inspection")
+		tables = [field.options for field in meta.get_table_fields()]
+		self.assertEqual(len(tables), 11)
+		for table in tables:
+			self.assertTrue(frappe.get_meta(table).istable, table)
