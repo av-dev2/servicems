3e0ea9fcb97c5f4fcb1f69fcef0e0a4584a974af
av-dev2 <emgani@aakvatech.com>
2026-08-26 13:14:19 +0300

    test: cover Service Pre Inspection Template

--- servicems/service_management/doctype/service_pre_inspection_template/test_service_pre_inspection_template.py
+++ servicems/service_management/doctype/service_pre_inspection_template/test_service_pre_inspection_template.py
@@ -1,9 +1,35 @@
 # Copyright (c) 2021, Aakvatech Limited and Contributors
 # See license.txt
 
-# import frappe
-import unittest
+import frappe
+
+from servicems.tests.base import ServiceMSTestCase
+from servicems.tests.utils import IGNORE_TEST_RECORD_DEPENDENCIES
+
+IGNORE_TEST_RECORD_DEPENDENCIES = IGNORE_TEST_RECORD_DEPENDENCIES
 
 
-class TestServicePreInspectionTemplate(unittest.TestCase):
-	pass
+class TestServicePreInspectionTemplate(ServiceMSTestCase):
+	def test_named_by_template_with_items(self):
+		doc = frappe.get_doc(
+			{
+				"doctype": "Service Pre Inspection Template",
+				"template_name": "_Test SMS Pre Inspection",
+				"pre_inspection_items": [
+					{"inspection_item": "Lights", "inspection_type": "Check"},
+					{"inspection_item": "Fuel level", "inspection_type": "Data", "option": "Litres"},
+				],
+			}
+		).insert()
+		self.assertEqual(doc.name, "_Test SMS Pre Inspection")
+		self.assertEqual([row.inspection_type for row in doc.pre_inspection_items], ["Check", "Data"])
+
+	def test_invalid_inspection_type_is_rejected(self):
+		doc = frappe.get_doc(
+			{
+				"doctype": "Service Pre Inspection Template",
+				"template_name": "_Test SMS Bad Type",
+				"pre_inspection_items": [{"inspection_item": "Lights", "inspection_type": "Toggle"}],
+			}
+		)
+		self.assertRaises(frappe.ValidationError, doc.insert)
