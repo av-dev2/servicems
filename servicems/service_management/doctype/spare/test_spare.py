bd7f5d25db89767f50c3fc6ec0d71859ebfeaec9
av-dev2 <emgani@aakvatech.com>
2026-08-26 13:14:25 +0300

    test: cover the Spare master

--- servicems/service_management/doctype/spare/test_spare.py
+++ servicems/service_management/doctype/spare/test_spare.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2022, Aakvatech Limited and Contributors
 # See license.txt
 
-# import frappe
-import unittest
+import frappe
+
+from servicems.tests.base import ServiceMSTestCase
+from servicems.tests.utils import IGNORE_TEST_RECORD_DEPENDENCIES, PARTS_ITEM_GROUP
+
+IGNORE_TEST_RECORD_DEPENDENCIES = IGNORE_TEST_RECORD_DEPENDENCIES
 
 
-class TestSpare(unittest.TestCase):
-	pass
+class TestSpare(ServiceMSTestCase):
+	def test_named_by_item_code(self):
+		spare = frappe.get_doc(
+			{
+				"doctype": "Spare",
+				"item_code": "_Test SMS Spare",
+				"item_group": PARTS_ITEM_GROUP,
+				"stock_uom": "Nos",
+			}
+		).insert()
+		self.assertEqual(spare.name, "_Test SMS Spare")
+
+	def test_group_and_uom_are_mandatory(self):
+		doc = frappe.get_doc({"doctype": "Spare", "item_code": "_Test SMS Spare 2"})
+		self.assertRaises(frappe.MandatoryError, doc.insert)
